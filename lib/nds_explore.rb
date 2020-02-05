555ef1e18ea5a40c211961c65ccbf985b5aafdea
Zac Uslianer <zacuslianer@gmail.com>
2020-02-05 22:11:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,11 +13,11 @@ def print_first_directors_movie_titles
 pretty_print_nds(directors_database)
 movies = directors_database[0][:movies]
 
-row_index = 0 
-while row_index < directors_database[0][:movies].count do 
+#row_index = 0 
+#while row_index < movies.count do 
   element_index = 0 
-  while element_index < directors_database[0][:movies][row_index].count do 
-    puts directors_database[0][:movies][row_index][element_index]
+  while element_index < movies[row_index].count do 
+    puts movies[row_index][element_index]
     element_index += 1 
     
 end
