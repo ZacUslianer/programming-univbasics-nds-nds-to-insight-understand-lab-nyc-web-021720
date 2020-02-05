2311f3ba42f70f985cd28650a57ad2f544ac1ef4
Zac Uslianer <zacuslianer@gmail.com>
2020-02-05 22:36:09 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -6,13 +6,13 @@ def directors_database
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
 
-movies = directors_database[0][:movies]
+# movies = directors_database[0][:movies]
 
 
-row_index = 0 
-while row_index < movies.count do 
-  puts movies[row_index][:title]
+# row_index = 0 
+# while row_index < movies.count do 
+#   puts movies[row_index][:title]
     
-row_index += 1
+# row_index += 1
 
-end
+# end
